4fe9e93854059a933d2b27f286b66e2bd5aff0c8
shyju-silcon <shyju@silcontechlabs.com>
2026-02-17 11:48:31 +0530

    Force add slug to package migration

---
+++ database/migrations/2026_02_17_061751_force_add_slug_to_packages_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasColumn('packages', 'slug')) {
+            Schema::table('packages', function (Blueprint $table) {
+                $table->string('slug')->nullable()->after('name');
+            });
+        }
+    }
+
+    public function down(): void
+    {
+        Schema::table('packages', function (Blueprint $table) {
+            $table->dropColumn('slug');
+        });
+    }
+};
