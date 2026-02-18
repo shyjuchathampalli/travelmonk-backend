d2c4cd6c799e5ec7aaaa0a4e0e1d3a3becc676b1
shyju-silcon <shyju@silcontechlabs.com>
2026-02-18 12:16:04 +0530

    Changed it in index also

--- app/Http/Controllers/Api/V1/PackageController.php
+++ app/Http/Controllers/Api/V1/PackageController.php
@@ -55,7 +55,7 @@ class PackageController extends Controller
                 'dayPlans' => function ($q) {
                     $q->orderBy('day_number')
                     ->with([
-                        'destination:id,name',
+                        'destination:id,name,image',
                         'activities:id,name'
                     ]);
                 }
